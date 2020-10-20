8d6b579a4fe0e6b0409db3e648f09049ac5a1da7
adDefraene <adriendefraene@gmail.com>
2020-10-20 12:23:47 +0200

    First commit - home.php template & css

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>POO 6 - Restaurant exercice</title>
+</head>
+<body>
+    <nav class="home">
+        <img src="images/mario_logo.png" alt="Restaurant's logo">
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">MENU</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a href="#">ORDER</a></li>
+        </ul>
+    </nav>
+    <header>
+        <img src="images/mario_logo_shaded.png" alt="Restaurant's logo">
+    </header>
+    <section id="section-home">
+        <div class="container">
+            <h1>WEEK'S SPECIALS</h1>
+            <div class="row">
+                <div class="card">
+                    <div class="card-row">
+                        <img src="images/lasagna.jpg" alt="Chef's lasagna">
+                        <a href="#">CHEF'S LASAGNA</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-row">
+                        <img src="images/pizza1.jpg" alt="Chef's lasagna">
+                        <a href="#">CHEF'S LASAGNA</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-row">
+                        <img src="images/scampis.jpg" alt="Chef's lasagna">
+                        <a href="#">CHEF'S LASAGNA</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <a href="#" class="button button-home">ORDER A MEAL</a>
+                <a href="#" class="link">REST OF THE MENU</a>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div id="footer-line1"></div>
+            <p>(c) EPSE LADP 2020 - WEBD2</p>
+            <div id="footer-line2"></div>
+        </div>
+    </footer>
+</body>
+</html>
